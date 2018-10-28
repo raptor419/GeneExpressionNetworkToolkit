578feb8f3ed244dc517766c11bf7d54e2821e3b7
devishi <devishi15024@iiitd.ac.in>
2018-10-29 04:53:34 +0530

    Update launch_application.R

--- launch_application.R
+++ launch_application.R
@@ -1,3 +1,15 @@
+#' @title ShinyApp
+#'
+#' @description Launch the ShinyApp
+#'
+#' @param 
+#'
+#' @return NULL
+#'
+#' @examples launch_application()
+#'
+#' @export
+
 launch_application <- function()
 {
   shiny::runApp(appDir = system.file(package = [GeneExpressionNetworkToolkit]))
