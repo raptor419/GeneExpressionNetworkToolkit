738b308e038466971ae674b94ff5f6704ef5e95d
devishi <devishi15024@iiitd.ac.in>
2018-11-03 15:36:04 +0530

    changes

--- R/launch_app.R
+++ R/launch_app.R
@@ -2,7 +2,7 @@
 #' @export
 launch_app <- function()
 {
-  input.good = paste(getwd(), "inst/GENT/GEOmetadb.sqlite", sep="/")
+  input.good = paste(getwd(), "GEOmetadb.sqlite", sep="/")
   if(!file_test("-f", input.good)){
     library(GEOmetadb)
     getSQLiteFile(destdir = getwd(), destfile = "GEOmetadb.sqlite.gz")
