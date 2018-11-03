482d7ef7e56e81e660b962976059380357cf1d9a
devishi <devishi15024@iiitd.ac.in>
2018-11-03 06:19:47 +0530

    Update R/launch_application.R

--- R/launch_application.R
+++ R/launch_application.R
@@ -2,5 +2,7 @@
 #' @export
 launch_application <- function()
 {
+  library(GEOmetadb)
+  getSQLiteFile(destdir = getwd(), destfile = "GEOmetadb.sqlite.gz")
   shiny::runApp(system.file('GENT', package = "GeneExpressionNetworkToolkit"))
 }
