f0763b0cebf1e1b6c75967fb0302e185d6c7818f
devishi <devishi15024@iiitd.ac.in>
2018-11-03 10:37:37 +0530

    Update R/launch_app.R

--- R/launch_app.R
+++ R/launch_app.R
@@ -6,8 +6,7 @@ launch_app <- function()
   m = paste(getwd(), "inst/GENT", sep="/")
   if(!file_test("-f", input.good)){
     library(GEOmetadb)
-    dirs = paste(getwd(), "inst/GENT", sep="/")
-    getSQLiteFile(destdir = dirs, destfile = "GEOmetadb.sqlite.gz")
+    getSQLiteFile(destdir = getwd(), destfile = "GEOmetadb.sqlite.gz")
   }
   setwd(m)
   shiny::runApp(system.file('GENT', package = "GeneExpressionNetworkToolkit"))
